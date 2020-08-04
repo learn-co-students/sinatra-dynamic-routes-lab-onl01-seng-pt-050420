463d116ecbf0f3e0113afb770254e6b8be382640
ryancdavey1 <ryancdavey1@gmail.com>
2020-08-04 10:43:25 -0600

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,19 @@ class App < Sinatra::Base
     "#{@reverse_name}"
   end
 
+  get '/square/:number' do
+    @square_number = params[:number].to_i ** 2
+    "#{@square_number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @full_string = []
+    @number.times do
+      @full_string << @phrase
+    end
+    "#{@full_string.join(" ")}"
+  end
+
 end
